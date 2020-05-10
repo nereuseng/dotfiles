82bdb4735e4b1df75b4ef3602a760bc5b5d963a4
nereuseng <nereuseng@gmail.com>
2020-05-10 16:18:51 +0800

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,100 @@
+{
+  "editor.dragAndDrop": true,
+  "editor.mouseWheelZoom": true,
+  "editor.minimap.enabled": true,
+  "workbench.iconTheme": "material-icon-theme",
+  "sync.gist": "1b35dff02b7b46ca0ca8d1191f01d547",
+  "sync.autoDownload": false,
+  "sync.autoUpload": false,
+  "sync.forceDownload": false,
+  "sync.quietSync": false,
+  "editor.renderControlCharacters": false,
+  "window.zoomLevel": 0,
+  "files.autoSave": "off",
+  "terminal.integrated.shell.windows": "C:\\Windows\\sysnative\\bash.exe",
+  // "terminal.integrated.shell.windows2": "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\PowerShell.exe",
+  "terminal.integrated.shellArgs.windows": ["-c", "zsh"],
+  "material-icon-theme.showUpdateMessage": false,
+  "terminal.integrated.fontFamily": "Meslo LG M for Powerline",
+  "git.autofetch": true,
+  "sync.removeExtensions": true,
+  "sync.syncExtensions": true,
+  "editor.tabSize": 2,
+  "prettier.tabWidth": 2,
+  "editor.snippetSuggestions": "top",
+  "extensions.autoUpdate": true,
+  // "local-history.exclude": [".gitignore"],
+  "terminal.integrated.rendererType": "dom",
+  "editor.fontFamily": "Monaco,'Noto Sans CJK TC Regular','Courier New', monospace",
+  "editor.lineHeight": 20,
+  "quokka.suppressExpirationNotifications": true,
+  "javascript.implicitProjectConfig.experimentalDecorators": true,
+  // "stylusSupremacy.insertBraces": false,
+  // "stylusSupremacy.quoteChar": "\"",
+  // "stylusSupremacy.insertColons": false,
+  // "stylusSupremacy.insertSemicolons": false,
+  // "stylusSupremacy.insertLeadingZeroBeforeFraction": false,
+  // "stylusSupremacy.selectorSeparator": ",\n",
+  "blueprint.templatesPath": ["./.vscode/blueprint-templates"],
+  "html.format.enable": false,
+  "[json]": {
+    "editor.formatOnSave": false,
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[jsonc]": {
+    "editor.formatOnSave": false,
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "explorer.confirmDelete": false,
+  "explorer.confirmDragAndDrop": false,
+  // "path-intellisense.mappings": {
+  //   "@": "${workspaceRoot}"
+  // },
+  "javascript.preferences.quoteStyle": "single",
+  "autoimport.semicolon": false,
+  "bracket-pair-colorizer-2.showBracketsInGutter": true,
+  "bracket-pair-colorizer-2.colorMode": "Independent",
+  "bracket-pair-colorizer-2.highlightActiveScope": true,
+  "editor.matchBrackets": false,
+  "bracket-pair-colorizer-2.activeScopeCSS": [
+    "backgroundColor : {color};  opacity: 0.5"
+  ],
+  "editor.cursorBlinking": "smooth",
+  "editor.cursorSmoothCaretAnimation": true,
+  "workbench.sideBar.location": "left",
+  "editor.suggest.snippetsPreventQuickSuggestions": false,
+  "workbench.colorTheme": "One Dark Pro",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  // "editor.formatOnSave": false,
+  // "[javascript]": {
+  //   "editor.defaultFormatter": "esbenp.prettier-vscode",
+  //   // "editor.formatOnSave": true
+  // },
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    // "editor.formatOnSave": true
+  },
+  "[typescript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    // "editor.formatOnSave": true
+  },
+  // "eslint.autoFixOnSave": true,
+  "typescript.updateImportsOnFileMove.enabled": "always",
+  "editor.fontSize": 14,
+  "vsicons.dontShowNewVersionMessage": true,
+  "terminal.integrated.shell.osx": "/bin/zsh",
+  "emmet.triggerExpansionOnTab": true,
+  "go.formatTool": "goimports",
+  "todo-tree.filtering.excludeGlobs": [
+    "node_modules/**/*"
+  ],
+  "git.confirmSync": false,
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  "todo-tree.tree.showScanModeButton": false,
+  "go.useLanguageServer": true,
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "prettier.trailingComma": "none",
+  "prettier.arrowParens": "avoid"
+}
